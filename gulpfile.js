5597f45ff521db2e47ab7a3423ce74a7fe80de87
Andriy Khimich <andrykhimich@gmail.com>
2019-06-08 09:40:36 +0300

    header

--- gulpfile.js
+++ gulpfile.js
@@ -67,8 +67,8 @@ gulp.task('js', function () {
   return gulp.src([
       path + 'libs/jquery/dist/jquery.min.js',
       path + 'libs/slick_1.9.0/slick/slick.min.js',
-      path + 'libs/odometer/odometer.min.js',
-      path + 'libs/select2/dist/js/select2.min.js',
+      // path + 'libs/odometer/odometer.min.js',
+      // path + 'libs/select2/dist/js/select2.min.js',
       path + 'libs/svg4everybody_2.1.9/svg4everybody.js'
       // path + 'libs/bootstrap/dist/js/bootstrap.bundle.min.js',
       // path + 'libs/bootstrap/dist/js/bootstrap.min.js'
