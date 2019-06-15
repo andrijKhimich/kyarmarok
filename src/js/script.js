4a30aff6e423d6aa3c2c2c3405ed14114aeb4f92
Andriy Khimich <andrykhimich@gmail.com>
2019-06-15 19:09:38 +0300

    product page done

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,91 @@
 $(document).ready(function () {
-
+  textHeight();
 
   // toggle filters
-  $('.filter__item>span').click(function(){
+  $('.filter__item>span').click(function () {
     $(this).siblings('.filter__list-sub').slideToggle();
   });
 
-  $('.goods-card__marks-box').click(function(){
+  $('.goods-card__marks-box').click(function () {
     $(this).toggleClass('goods-card__marks-box_active');
   });
 
-  $('.pagination__link').click(function(e){
+  $('.pagination__link').click(function (e) {
     e.preventDefault();
     $(this).toggleClass('pagination__link_active');
   });
 
-  $('.sidebar__button_mob').click(function(e){
+  $('.sidebar__button_mob').click(function (e) {
     e.preventDefault();
     $('.js-sidebar_left').toggleClass('sidebar_left_active');
     $('.sidebar__overlay').toggleClass('sidebar__overlay_active');
   });
-  $('.sidebar__overlay').click(function(){
-    console.log('dsf');
+
+
+
+  $('.sidebar__overlay').click(function () {
     $('.js-sidebar_left').removeClass('sidebar_left_active');
     $('.sidebar__overlay').removeClass('sidebar__overlay_active');
   });
-  
+
+  $('.product-tabs__list').on('click', '.product-tabs__item:not(.product-tabs__item_active)', function () {
+    $(this)
+      .addClass('product-tabs__item_active').siblings().removeClass('product-tabs__item_active')
+      .closest('div.product-tabs').find('div.product-tabs__content').hide().eq($(this).index()).fadeIn(400);
+  });
+
+
+
+  $('.product-slider_sm-content .slick-slide').click(function () {
+    var slideId = $(this).attr('data-slick-index');
+    moveSlidersTo(slideId);
+  });
+
+
+
+  // slider how works navigation 'prew'
+  $('.js-sm-prev').click(function (e) {
+    e.preventDefault();
+    var curentSlideId = $('.product-slider_sm-content .slick-current').attr('data-slick-index');
+    moveSlidersTo(parseInt(curentSlideId) - 1);
+  });
+
+  // slider how works navigation 'next'
+  $('.js-sm-next').click(function (e) {
+    e.preventDefault();
+    var curentSlideId = $('.product-slider_sm-content .slick-current').attr('data-slick-index');
+    moveSlidersTo(parseInt(curentSlideId) + 1);
+  });
+
+  function moveSlidersTo(idSlide) {
+    $('.product-slider_sm-content').slick('slickGoTo', idSlide);
+    $('.product-slider__img').slick('slickGoTo', idSlide);
+  }
+
   // END DOCUMENT READY
 })
 
+// checkind window sizes
+$(window).resize(function () {
+  textHeight();
+});
 
-
+function textHeight() {
+  var maxContent = 0;
+  $('.js-height').each(function () {
+    if ($(this).height() > maxContent) {
+      maxContent = $(this).height();
+      $('.js-height').height(maxContent);
+    } else if ($(window).width() <= 1199) {
+      $('.js-height').css('height', 'auto');
+    }
+  });
+}
 
 $('.filter__list-sub').hide();
 
 
-// checkind window sizes
-$(window).resize(function () {
 
-});
 
 
 
@@ -51,31 +98,76 @@ $(window).resize(function () {
 //   }
 // });
 
-
-
-
-
 $('.js-slider').slick({
   dots: true,
   dotsClass: 'main-slider__pagination',
   infinite: true,
   speed: 500,
   fade: true,
-  // appendDots: 'main-slider__pagination',
+  appendDots: 'main-slider__pagination',
   pauseOnDotsHover: true,
   cssEase: 'linear',
   arrows: true,
   prevArrow: $('.js-prev'),
   nextArrow: $('.js-next'),
-  // customPaging: function(slider, i) {
-  //   // this example would render "tabs" with titles
-  //   return '<li class="pager"></li>';
-  // },
-  // customPaging: function(slider, i) { 
-  //   return '<li class="pager">' + $(slider.$slides[i]).attr('title') + '</li>';
-// },
-
-  
 });
+
+
+
+$('.product-slider__img').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  adaptiveHeight: true,
+  // arrows: true,
+  prevArrow: $('.js-sm-prev'),
+  nextArrow: $('.js-sm-next'),
+});
+
+$('.product-slider_sm-content').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 500,
+  fade: false,
+  cssEase: 'linear',
+  adaptiveHeight: true,
+  // arrows: true,
+  prevArrow: $('.js-sm-prev'),
+  nextArrow: $('.js-sm-next'),
+  responsive: [{
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.photo-reviews-slider').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 500,
+  fade: false,
+  cssEase: 'linear',
+  adaptiveHeight: true,
+  arrows: true,
+  prevArrow: $('.js-photo-prev'),
+  nextArrow: $('.js-photo-next'),
+});
+
 // Load SVG-sprite for all browsers
 svg4everybody({});
